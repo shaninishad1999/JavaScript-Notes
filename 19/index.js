f4c5e536c2d7f89be20b30c7d74284ffca64e88c
shaninishad1999 <shaninishad1999@gmail.com>
2024-07-20 01:16:03 +0530

    function in js

---
+++ 19/index.js
@@ -0,0 +1,52 @@
+// function demo(){
+//     return "Hello";
+// }
+// console.log(demo());
+
+// function demo(a) {
+//     return a;
+// }
+// let a = 20;
+// console.log(demo(a));
+
+// function deepak(c){
+
+// console.log()
+// }
+// let age=32;
+// deepak(age);
+
+// wap to take input from the user that number using function
+
+
+// function tbl(n){
+//     for(let i=1;i<=10;i++)
+//     console.log(n*i);
+// }
+
+// let n=parseInt(prompt("Enter any Number"));
+// tbl(n);
+
+
+
+// "use strict";
+
+// function tbl( n) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(n * i);
+//     }
+// }
+
+// let n = parseInt(prompt("Enter any number:"));
+// tbl(n);
+
+// wap to take input form user and print cube of a number 
+// using with argument and with vlue
+
+
+function tbl( n) {
+   return n*n*n;
+}
+
+let n = parseInt(prompt("Enter any number:"));
+console.log(tbl(n));
