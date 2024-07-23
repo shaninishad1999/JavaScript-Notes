8c0ad338916d9dc35ae62a4684c457b1a3432838
shaninishad1999 <shaninishad1999@gmail.com>
2024-07-23 07:02:34 +0530

    updated

---
+++ 22/index.js
@@ -0,0 +1,18 @@
+//taking something and returning something
+// let a=10;
+// let b=(a)=>{return a*a};
+// console.log(b(a));
+
+// wap to input form the user chaek number is 
+// even or odd and return  of square
+
+// let a=parseInt(prompt("Enter a number "))
+// let a=10;
+// let b=(a)=>{
+//  return 3.14*a*a;
+//    };
+// console.log(b(a));
+
+// let a=parseInt(prompt("Enter a number "))
+// let b=(a)=>(a*a);
+// console.log(b(a))
