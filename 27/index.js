85d2dca7997aec46d847c2b8d0bd9c9072ac5936
shaninishad1999 <shaninishad1999@gmail.com>
2024-07-28 20:53:12 +0530

    27

---
+++ 27/index.js
@@ -0,0 +1,29 @@
+// setTimeout(()=>{alert("this working ")},200)
+
+
+// let date=new Date();
+// // console.log(date)
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.toDateString())
+// console.log(date.getDate())
+// console.log(date.getDay())
+// console.log(date.getFullYear())
+// console.log(date.getHours())
+// console.log(date.getUTCDate())
+// console.log(date.valueOf())
+// console.log(date.toString())
+// console.log(date.toJSON())
+
+
+// setInterval(()=>{
+//     let date=new Date()
+//     let time=date.toLocaleTimeString()
+//     let st=document.getElementById('head')
+//     st.innerHTML=time
+// },1000)
+
+
+
+//events
+
