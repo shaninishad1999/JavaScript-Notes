d4361fc0352c1d52bc07da8a5b698b115965d113
shaninishad1999 <shaninishad1999@gmail.com>
2024-08-09 22:28:30 +0530

    math module

---
+++ math 38/script.js
@@ -0,0 +1,58 @@
+// let x=Math.sqrt(9);
+// console.log(x);
+// let d=document.getElementById("demo").innerHTML=x;
+// // document.write(x);
+// alert(x);
+
+
+// sign returns 1 if number is positive ,-1 if number is negative
+// 0 if numbe is 0 
+// let x=Math.sign(10);
+// let y=Math.sign(-45);
+// let z=Math.sign(0);
+// alert(x);
+// alert(y);
+// alert(z);
+
+
+// power of number (number,power)
+// let x=Math.pow(4,3)
+// alert(x)
+
+// abs return absolute value means mod
+// let x=Math.abs(-45);
+// console.log(x)
+
+// // round figure value
+// let x=Math.round(6.4);
+// alert(x)
+
+// // return upper higer value
+// let x=Math.ceil(-6.4);
+// alert(x)
+
+// return floor value or lower value
+// let x=Math.floor(6.7);
+// alert(x)
+
+
+// return minimum value return karege
+// let x=Math.min(4,8,5);
+// alert(x)
+
+// let x=Math.max(4,8,5);
+// alert(x)
+
+// remove the decimal number and give integer value
+// let x=Math.trunc(4.5888)
+// alert(x);
+
+//rand number between 0(inclusive ) and 
+// let x=Math.random()*100 for decimal
+
+
+let x=Math.floor(Math.random()*100)
+
+alert(x);
+
+
